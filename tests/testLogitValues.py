8430a14ced85584544de27b7457a884b51d7cc7f
New-10 <newton143@gmail.com>
2024-11-03 18:42:48 -0600

    11/2
    11/2

--- tests/testLogitValues.py
+++ tests/testLogitValues.py
@@ -29,7 +29,8 @@ class TestCase(unittest.TestCase):
       y = data.loc[:100, 'white']
       reg = RegressionModel(x, y, create_intercept=True, regression_type='logit')
       reg.fit_model()
-
+      reg.summary()
+      
       sex = {'coefficient': -1.1229156890097627,
       'standard_error': 0.39798772782618025,
       'z_stat': -2.821483202869492,
